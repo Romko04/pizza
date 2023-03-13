c44501cfe81ab3940b4f5f3b4d43217fb63e4e78
Roman <kurpelroma@gmail.com>
2023-03-13 21:01:53 +0200

    git

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <Provider store={store}>
-      <BrowserRouter >
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
         <App />
       </BrowserRouter>
     </Provider>
