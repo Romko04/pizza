9aca9d128b32f3094968254ef1f71d0d532ea973
Roman <kurpelroma@gmail.com>
2023-03-13 21:13:03 +0200

    git

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
@@ -10,9 +10,9 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL} >
+      <HashRouter >
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
